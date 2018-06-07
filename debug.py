701f80d7435956de3b0e411a39427159bb7eda7c
python <25702166+wCmJ@users.noreply.github.com>
2018-06-07 12:24:16 +0800

    Create debug.py

---
+++ debug.py
@@ -0,0 +1,12 @@
+#print
+  
+#assert
+n=0
+assert n != 0, 'n is zero!'
+#python debug.py will execute assert.
+#python -O debug.py will discard assert, treat it as pass. -O will close assert.
+
+
+
+
+
