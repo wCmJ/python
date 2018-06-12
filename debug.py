1dee7a1fe8c645d335c6890934bc47cd9ca4df45
python <25702166+wCmJ@users.noreply.github.com>
2018-06-12 14:08:53 +0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -6,7 +6,22 @@ assert n != 0, 'n is zero!'
 #python debug.py will execute assert.
 #python -O debug.py will discard assert, treat it as pass. -O will close assert.
 
+#logging
+import logging
+#specify level of debug
+logging.basicConfig(level=logging.INFO/DEBUG/ERROR/WARNING)
+logging.info/debug/error/warning('n=%d' % n)
 
+#pdb
+import pdb
+#python -m pdb debug.py
+#l check code
+#p variable check value
+#n run line by line
+#q quit pdb
 
-
+pdb.set_trace()
+#python debug.py
+#p variable 
+#c continue to run
 
