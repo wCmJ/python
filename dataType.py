1098fcd069a476605ef318e909bb6109a584ddb6
python <25702166+wCmJ@users.noreply.github.com>
2018-07-04 19:15:46 +0800

    Create dataType.py

---
+++ dataType.py
@@ -0,0 +1,37 @@
+#Numbers: int, float, complex
+print(type(1))
+<class 'int'>
+print(type(1.0))
+<class 'float'>
+print(type(1+2.0j))
+<class 'complex'>
+
+#list
+print(type([]))
+<class 'list'>
+
+
+#tuple
+print(type(()))
+<class 'tuple'>
+
+#strings
+print(type('1'))
+<class 'str'>
+
+#set
+print(type({1}))
+<class 'set'>
+
+#dictionary
+print(type({}))
+<class 'dict'>
+
+#generator
+print(type((x for x in range(1,10))))
+<class 'generator'>
+
+#function
+<class 'function'>
+
+
