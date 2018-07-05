30d6b074dcef874089477799bf6ef83ed33e3845
python <25702166+wCmJ@users.noreply.github.com>
2018-07-05 12:40:13 +0800

    Update dataType.py

--- dataType.py
+++ dataType.py
@@ -30,8 +30,65 @@ print(type({}))
 #generator
 print(type((x for x in range(1,10))))
 <class 'generator'>
+except: StopIteration
 
 #function
 <class 'function'>
 
+
+
+'''
+Iterable: 
+    1.list, tuple, dict, set, str
+        iter(list/tuple/dict/set/str)   --->    Iterator
+                            
+    
+                            
+    2.生成器   <----->    Iterator(can be invoked by next() and returned value one by one) 
+        ||
+      generator
+        ||
+    3.function with yield   <----->    Iterator
+'''
+
+'''
+Actually, for in python is same as next().
+for x in [1,2,3,4,5]:
+    pass
+    
+equal with:
+it = iter([1,2,3,4,5])
+while True:
+    try:
+        x = next(it)
+    except StopIteration:
+        break
+'''        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
